4685d400038e2a41308e72a8cdf6dbed8ee76ba2
Max Base <MaxBaseCode@Gmail.Com>
2019-08-26 01:05:41 +0430

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,17 @@
+# https://github.com/BaseMax/SimpleDjangoSignals
+from django.db import models
+from django.db.models import signals
+
+class book(models.Model):
+	name = models.CharField(max_length=50)
+	author = models.CharField(max_length=25)
+	code = models.CharField(max_length=9)
+
+def bookCreated(sender, instance, created, **kwargs):
+	print "Save is called"
+
+signals.post_save.connect(receiver=bookCreated, sender=book)
+
+# obj = book(name='book1', author='user1', code='123456')
+# ...
+# obj.save()
